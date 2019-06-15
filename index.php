584e285df29bb3937f3b6e25729129cfeab29826
Tristan Martin <smove50@gmail.com>
2019-06-14 20:38:04 -0400

    new php updates

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php header( 'Location: /index.html' ) ; ?>
+
+<?php 
+// the message added
+$msg = "first line of text\nsecond line of text";
+
+//use woldwrap () if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+//send email
+mail("tristanmartin88@gmail.com", "My subject", $msg);
+?>
